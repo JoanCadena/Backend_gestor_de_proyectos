cd815a607f1ad7d2132ea38c3c10e4e43acd7553
Isaac Diaz <isaac.diazc@IsaacDiaz.local>
2022-04-03 01:48:28 -0500

    test compromisos

--- test/crear_compromisos.test.js
+++ test/crear_compromisos.test.js
@@ -1,12 +1,22 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe("GET compromisos", () => {
-    it("Debe retornar un JSON ", done => {
+describe("Endpoint para compromisos", () => {
+    it("GET ALL compromisos", done => {
         request(app)
             .get('/compromiso')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200, done)
     });
+
+    it('Devuelve un mensaje de error', done => {
+        request(app).get('/compromisos/noexistis')
+        .set('Accept', 'application/json')
+        .expect(404)
+        .end(err => {
+          if (err) return done(err);
+          done();
+        })
+})
 });
