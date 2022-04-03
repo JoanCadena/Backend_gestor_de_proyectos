7c4456557e94650826f91423cef9740fd338cb09
JoanCadena <inmortal_20@live.com>
2022-04-02 23:07:27 -0500

    Modificacion test crear_compromisos

--- test/crear_compromisos.test.js
+++ test/crear_compromisos.test.js
@@ -2,15 +2,18 @@ const request = require('supertest');
 const app = require('../app');
 
 describe("GET compromisos", () => {
-    it("Debe retornar un JSON", async () => {
-        await request(app).get('/compromiso')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200,function(err,res){
+    it("Debe retornar un JSON ", done => {
+        request(app)
+            .get('/compromiso')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200,function(err,res){
             if(err){
-                console.log(err)
+                //console.log(err)
+                done(err)
             }else{
-                console.log(res.body)
+                //console.log(res.body)
+                done()
             }
         })
     });
