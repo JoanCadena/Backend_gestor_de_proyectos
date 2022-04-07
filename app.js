eeb146145bbbcac719ae2ede0c4c5ea8b1683cb0
JoanCadena <inmortal_20@live.com>
2022-04-07 18:57:35 -0500

    correccion puerto

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const rutas_archivos = require("./routes/Anexar_Archivo")
 app.use(rutas_compromisos, rutas_espacios, rutas_integrantes, rutas_investigadores, rutas_archivos);
 
 //Puerto
-const PORT2 = config.PORT;
+const PORT = config.PORT || 3001;
 
 //Levantar el servidor para escuchar los puertos
 app.listen(PORT, () => {
